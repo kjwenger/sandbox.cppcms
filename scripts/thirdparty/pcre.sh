6bd2d8e696980f6c6205797bdcd11d2b9d938f83
Klaus Wenger <kjwenger@yahoo.com>
2017-07-15 21:12:09 +0200

    pcre for linux

--- scripts/thirdparty/pcre.sh
+++ scripts/thirdparty/pcre.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -x
+
 CPUS=$(lscpu | grep "^CPU(s):" | sed s/"CPU(s):                "//)
 
 CURRENT_DIR="$(pwd)"
@@ -10,13 +12,12 @@ PROJECT_DIR="$(dirname "${SCRIPTS_DIR}")"
 USR_DIR="${PROJECT_DIR}/usr"
 
 cd "${THIRDPARTY_DIR}"
+sudo rm -Rf pcre
 svn co svn://vcs.exim.org/pcre/code/trunk pcre
 cd pcre
 mkdir -p build
 cd build
-#del /s /f /q *.*
-cmake -G "MinGW Makefiles" \
-      -DCMAKE_INSTALL_PREFIX="%USR_DIR%" \
+cmake -DCMAKE_INSTALL_PREFIX="${USR_DIR}" \
       ..
 make -j ${CPUS}
 make -j ${CPUS} install
