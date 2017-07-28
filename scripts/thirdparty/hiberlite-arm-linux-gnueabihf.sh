7b02ba67961ad10cb784fcdb96d4c3a76efc89b2
Klaus Wenger <kjwenger@yahoo.com>
2017-07-28 20:58:18 +0200

    added hiperlite for armhf

--- scripts/thirdparty/hiberlite-arm-linux-gnueabihf.sh
+++ scripts/thirdparty/hiberlite-arm-linux-gnueabihf.sh
@@ -18,10 +18,12 @@ sed -i 's/INSTALL_PREFIX = \/usr/INSTALL_PREFIX ?= \/usr/g' Makefile
 sed -i 's/gcc/$(CC)/g' Makefile
 sed -i 's/g++/$(CXX)/g' Makefile
 sed -i.old '1s/^/CC ?= gcc\nCXX ?= g++/' Makefile
-INSTALL_PREFIX="${USR_DIR}"
-make -j ${CPUS} clean INSTALL_PREFIX="${INSTALL_PREFIX}"
-make -j ${CPUS} INSTALL_PREFIX="${INSTALL_PREFIX}"
-make -j ${CPUS} install INSTALL_PREFIX="${INSTALL_PREFIX}"
+INSTALL_PREFIX="${USR_DIR}/arm-linux-gnueabihf"
+CC=/usr/bin/arm-linux-gnueabihf-gcc
+CXX=/usr/bin/arm-linux-gnueabihf-g++
+make -j ${CPUS} clean   INSTALL_PREFIX="${INSTALL_PREFIX}" CC="${CC}" CXX="${CXX}"
+make -j ${CPUS}         INSTALL_PREFIX="${INSTALL_PREFIX}" CC="${CC}" CXX="${CXX}"
+make -j ${CPUS} install INSTALL_PREFIX="${INSTALL_PREFIX}" CC="${CC}" CXX="${CXX}"
 popd
 popd
 cd "${CURRENT_DIR}"
