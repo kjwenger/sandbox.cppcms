06c5ebdb99e7084f96e8e32327e8978c15d21d1f
Klaus Wenger <kjwenger@yahoo.com>
2017-08-06 16:17:03 +0200

    adjusted script dir

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-PARENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-SCRIPTS_DIR="$(dirname "${PARENT_DIR}")"
+SCRIPTS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PROJECT_DIR="$(dirname "${SCRIPTS_DIR}")"
 
 cd "${PROJECT_DIR}"
