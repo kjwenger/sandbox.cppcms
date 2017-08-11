e327d4bf16383f494e5057a3156be291c2fc5855
Klaus Wenger <kjwenger@yahoo.com>
2017-08-11 19:24:02 +0200

    cleaning up scripts

--- scripts/setup/prerequisites.sh
+++ scripts/setup/prerequisites.sh
@@ -16,7 +16,7 @@ sudo apt install build-essential -y
 sudo apt install gcc -y
 sudo apt install g++ -y
 sudo apt install cmake -y
-sudo apt install cppcheck
+sudo apt install cppcheck -y
 
 # Get CppCMS prerequisites
 sudo apt install libpcre3-dev -y
