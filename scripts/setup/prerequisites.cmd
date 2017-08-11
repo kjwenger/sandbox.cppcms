90af26ad9146781519f9dce56733e8964eb2b33d
Klaus Wenger <kjwenger@yahoo.com>
2017-08-11 19:38:54 +0200

    Merge branch 'part/01' into part/02
    # Conflicts:
    #	scripts/setup/prerequisites.cmd
    #	scripts/setup/prerequisites.sh
    #	src/main.cpp

--- scripts/setup/prerequisites.cmd
+++ scripts/setup/prerequisites.cmd
@@ -1,2 +1,14 @@
-choco install cmake
-choco install mingw
+@rem general tools
+choco install unzip -y
+
+@rem network tools
+choco install curl -y
+choco install wget -y
+
+@rem version control tools
+choco install git -y
+choco install subversion -y
+
+@rem Get build tools
+choco install cmake -y
+choco install mingw -y
